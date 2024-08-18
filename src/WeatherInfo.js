146ca17fbc370f5080282b6ec4680367b979d37a
EBroere <e.broere91@gmail.com>
2024-08-18 21:24:55 +0200

    improve styling css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -25,23 +25,17 @@ export default function WeatherInfo(props) {
               Humidity: <strong>{props.data.humidity}%</strong>, Wind:{" "}
               <strong>{props.data.wind}km/h</strong>,
             </li>
-
-            <li>
-              Feels Like: <strong>props.data.feels_like</strong>, Pressure:
-              <strong>props.data.pressure</strong>,
-            </li>
-            <li>
-              Coordinates: lat.{" "}
-              <strong>props.data.longitude</strong>, long.{" "}
-              <strong>props.data.latitude</strong>.
-            </li>
           </ul>
         </div>
 
         <div className="col-lg-6 col-md-6 col-sm-8">
           <div className="temperature-container d-flex justify-content-end">
             <div className="icon-wrapper">
-              <WeatherIcon code={props.data.icon} size={100} />
+              <img
+                src={props.data.icon}
+                alt={props.data.description}
+                className="float-left"
+              />
             </div>
             <div>
               <WeatherTemperature celsius={props.data.temperature} />
