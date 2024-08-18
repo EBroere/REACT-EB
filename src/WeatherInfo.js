542d7061b6c329408284df2a6bbf5c96615ca219
EBroere <e.broere91@gmail.com>
2024-08-18 16:41:51 +0200

    build the react weather app

---
+++ src/WeatherInfo.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <div className="col-6">
+          <h1>{props.data.city}</h1>
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />, {props.data.description}
+            </li>
+            <li>
+              Humidity: <strong>{props.data.humidity}%</strong>, Wind:{" "}
+              <strong>{props.data.wind}km/h</strong>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-lg-6 col-md-6 col-sm-8">
+          <div className="temperature-container d-flex justify-content-end">
+            <div className="icon-wrapper">
+              <WeatherIcon code={props.data.icon} size={100} />
+            </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
